e774c798880de9549bc337636ac786b32cc88acb
Bastien Abadie <bastien@nextcairn.com>
2016-07-08 17:08:59 +0200

    Load & display bugs

--- test.elm
+++ test.elm
@@ -18,16 +18,23 @@ main =
 init : Int -> (Analysis , Cmd Msg)
 init analysis_id =
   (
-    Analysis analysis_id "nope",
+    Analysis analysis_id "nope" [],
     fetchAnalysis analysis_id 
   )
 
 -- Model
 
+type alias Bug = {
+  id: Int,
+  bugzilla_id: Int,
+  summary: String
+}
+
 type alias Analysis = {
-    id: Int,
-    name: String
-  }
+  id: Int,
+  name: String,
+  bugs: List Bug
+}
 
 -- Update
 
@@ -57,22 +64,41 @@ fetchAnalysis id =
 
 decodeAnalysis : Decoder Analysis
 decodeAnalysis = 
-  Json.object2 Analysis
+  Json.object3 Analysis
     ("id" := Json.int)
     ("name" := Json.string)
+    ("bugs" := Json.list decodeBug)
+
+decodeBug : Decoder Bug
+decodeBug =
+  Json.object3 Bug
+    ("id" := Json.int)
+    ("bugzilla_id" := Json.int)
+    (Json.at ["payload", "bug", "summary"] Json.string)
 
 -- Subscriptions
-
 
 subscriptions : Analysis -> Sub Msg
 subscriptions analysis =
   Sub.none
 
--- View
+-- Views
 
 view : Analysis -> Html Msg
 view analysis =
   div []
     [
-      h1 [] [text ("Analysis: " ++ analysis.name)]
+      h1 [] [text ("Analysis: " ++ analysis.name)],
+      div [] (List.map viewBug analysis.bugs)
     ]
+
+viewBug: Bug -> Html msg
+viewBug bug =
+  div [class "bug"] [
+    h4 [] [text bug.summary],
+    p [] [
+      span [] [text ("#" ++ (toString bug.bugzilla_id))],
+      a [href ("https://bugzilla.mozilla.org/show_bug.cgi?id=" ++ (toString bug.bugzilla_id)), target "_blank"] [text "View on bugzilla"]
+    ],
+    hr [] []
+  ]
